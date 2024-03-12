da71a1fe5571b35e7a42b0bfb82f4a7d6ed5ba7b
KonstantinovIKM723a <alex7wrt@gmail.com>
2024-03-12 16:47:40 +0200

    Update

--- lab8_2_2/lab8_2_2.cpp
+++ lab8_2_2/lab8_2_2.cpp
@@ -7,52 +7,22 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    const int n = 20;
-    float a[n];
+    const int N = 6;
+    int a[N] = { 0, 0, 0, 0, 0, 0 };
+    int A = 1000000;
+    int i = 0;
 
-    srand(time(NULL));
+    //Ввод числа 0 <= A <= 999999
+    while (A < 0 || A > 999999) cin >> A;
 
-    //Ініціалізуємо масив дійсних чисел довжиною n елементів значеннями від -10 до 10
-    for (int i = 0; i < n; i++) a[i] = float(rand() % 2001 - 1000) / 100;
+    //Заповнення масиву цифрами числа А, починаючи з останньої цифри числа
+    while (A > 0) {
+        a[i] = A % 10;
+        A = A / 10;
+        i++;
+    }
 
-    //Виводимо масив на екран
+    //Виведення елементів масива
     cout << "Елементи масиву: ";
-    for (int i = 0; i < n; i++) cout << a[i] << " ";
-    cout << endl;
-
-    //а) кількість і добуток від'ємних елементів
-    int negative_N = 0;
-    float negative_P = 1;
-
-    for (int i = 0; i < n; i++) {
-        if (a[i] < 0) {
-            negative_N++;
-            negative_P *= a[i];
-        }
-    }
-
-    //Виводимо результат на екран
-    cout << "кількість від'ємних елементів: " << negative_N << endl;
-    if (negative_N == 0) {
-        cout << "Від'ємних елементів нема" << endl;
-    }
-    else cout << "добуток від'ємних елементів: " << negative_P << endl;
-
-    //б) сума елементів масиву, розташованих між першим і другим додатними елементами.
-    int positive_N = 0;
-    float positive_S = 0;
-
-    for (int i = 0; i < n; i++) {
-        if (a[i] > 0) {
-            positive_N++;
-            if (positive_N == 2) break;
-        }
-        else if (positive_N == 1) positive_S += a[i];
-    }
-
-    //Виводимо результат на екран
-    if (positive_N == 2) {
-        cout << "Сума додатних елементів, розташованих між першим і другим додатними елементами: " << positive_S;
-    }
-    else cout << "Додатних елементів менше двох";
+    for (int i = 0; i < N; i++) cout << a[i] << " ";
 }
